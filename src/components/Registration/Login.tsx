240d3c32db95cd6045f93d4cd7f237085e9fcab5
maleklobaskilubi <erik.rass@student.tuke.sk>
2025-03-25 22:57:26 +0100

    updating login.tsx

--- src/components/Registration/Login.tsx
+++ src/components/Registration/Login.tsx
@@ -1,55 +1,92 @@
-const Login = () => {
-    return (
-      <div className="flex w-full max-w-full mx-12 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-[3750px]">
-        <div 
-          className="hidden bg-cover lg:block lg:w-1/4" 
-          style={{ backgroundImage: "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')" }}
-        ></div>
-        
-        <div className="w-full px-80 py-10 md:px-40 lg:w-3/4">
-          <div className="flex justify-center mx-auto">
-            <img className="w-auto h-9 sm:h-11" src=" " alt="Logo" />
-          </div>
-  
-          <p className="mt-3 text-xl text-center text-gray-600 dark:text-gray-200">Welcome back!</p>
-  
-          <button className="flex items-center justify-center w-full mt-4 text-base text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <div className="px-4 py-2">
-              <svg className="w-8 h-8" viewBox="0 0 40 40">
-                <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
-              </svg>
+import { useState } from "react";
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <section className="max-w-3xl  flex box-border justify-center items-center">
+      <div className="bg-[#dfa674] rounded-2xl flex max-w-3xl p-5 items-center">
+        <div className="md:w-1/2 px-8">
+          <h2 className="font-bold text-3xl text-[#002D74]">Login</h2>
+          <p className="text-sm mt-4 text-[#002D74]">If you already a member, easily log in now.</p>
+
+          <form action="" className="flex flex-col gap-4">
+            <input className="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email" />
+            <div className="relative">
+              <input 
+                className="p-2 rounded-xl border w-full" 
+                type={showPassword ? "text" : "password"} 
+                name="password" 
+                id="password" 
+                placeholder="Password" 
+              />
+              {!showPassword ? (
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="gray" 
+                  className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer z-20 opacity-100"
+                  viewBox="0 0 16 16"
+                  onClick={togglePasswordVisibility}
+                >
+                  <path
+                    d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z">
+                  </path>
+                  <path
+                    d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z">
+                  </path>
+                </svg>
+              ) : (
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="currentColor"
+                  className="bi bi-eye-slash-fill absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer z-20"
+                  viewBox="0 0 16 16"
+                  onClick={togglePasswordVisibility}
+                >
+                  <path
+                    d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z">
+                  </path>
+                  <path
+                    d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z">
+                  </path>
+                </svg>
+              )}
             </div>
-            <span className="w-5/6 px-4 py-3 font-bold text-center">Sign in with Google</span>
+            <button className="bg-[#002D74] text-white py-2 rounded-xl hover:scale-105 duration-300 hover:bg-[#206ab1] font-medium" type="submit">Login</button>
+          </form>
+          <div className="mt-6 items-center text-gray-100">
+            <hr className="border-gray-300" />
+            <p className="text-center text-sm">OR</p>
+            <hr className="border-gray-300" />
+          </div>
+          <button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium">
+            <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
+              <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
+              <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
+              <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path>
+              <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+            </svg>
+            Login with Google
           </button>
-  
-          <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
-            <span className="text-sm text-center text-gray-500 uppercase dark:text-gray-400">or login with email</span>
-            <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
-          </div>
-  
-          <div className="mt-4">
-            <label className="block mb-2 text-base font-medium text-gray-600 dark:text-gray-200">Email Address</label>
-            <input className="block w-full px-5 py-2.5 text-base text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300" type="email" />
-          </div>
-  
-          <div className="mt-4">
-            <label className="block mb-2 text-base font-medium text-gray-600 dark:text-gray-200">Password</label>
-            <input className="block w-full px-5 py-2.5 text-base text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300" type="password" />
-          </div>
-  
-          <div className="mt-6">
-            <button className="w-full px-8 py-3 text-base font-medium tracking-wide text-white bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300">Sign In</button>
-          </div>
-  
-          <div className="flex items-center justify-between mt-4">
-            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
-            <span className="text-sm text-gray-500 uppercase dark:text-gray-400">or sign up</span>
-            <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+          <div className="mt-10 text-sm border-b border-gray-500 py-5 playfair tooltip">Forget password?</div>
+
+          <div className="mt-4 text-sm flex justify-between items-center container-mr">
+            <p className="mr-3 md:mr-0 ">If you don't have an account..</p>
+            <button className="hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300">Register</button>
           </div>
         </div>
+        <div className="md:block hidden w-1/2">
+          <img className="rounded-2xl max-h-[1600px]" src="https://images.unsplash.com/photo-1552010099-5dc86fcfaa38?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxmcmVzaHxlbnwwfDF8fHwxNzEyMTU4MDk0fDA&ixlib=rb-4.0.3&q=80&w=1080" alt="login form image" />
+        </div>
       </div>
-    );
-  };
-  
-  export default Login;
+    </section>
+  );
+}
