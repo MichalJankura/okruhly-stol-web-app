956b6722095176e48805dcd38f1e92f33c193731
kurka <72251818+MichalJankura@users.noreply.github.com>
2025-03-31 15:30:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SimpleBlog from './components/SimpleBlog';
 import MainHero from './components/MainHero';
 import Navbar from './components/Navbar';
